56270a7deb83fc806299dc89583a0eb5a6f554dc
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-01-10 23:36:26 +0500

    feat:Set up Vercel configuration, update app greeting, and enable CORS in main application file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Allow CORS (important for backend APIs)
+  app.enableCors({
+    origin: '*', // Allow all origins (or specify a list of allowed origins)
+  });
+
+  // Use Vercel's PORT environment variable or default to 3000
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
