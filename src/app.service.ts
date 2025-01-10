7f4fb2a37b70da5b9e10e1d10a0304e583bde375
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-01-10 23:56:16 +0500

    fix: Correct greeting punctuation in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'WelCome to task have back-end API';
+    return 'WelCome to task have back-end API!';
   }
 }
